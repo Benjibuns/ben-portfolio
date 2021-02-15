fbb00d0327d6ffab6829de7552f5952bdbce5703
Benjamin Rivera <benriveracoding@gmail.com>
2021-02-15 16:26:50 -0700

    Introduction to React Components

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import PortfolioContainer from './portfolio-container';
 
 export default class App extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class App extends Component {
         <h1>Ben Rivera's Portfolio</h1>
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
+          <PortfolioContainer/>
         </div>
       </div>
     );
