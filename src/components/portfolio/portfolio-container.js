34bd912d9f1739f655adbe016a2b9d6576af14fe
Benjamin Rivera <benriveracoding@gmail.com>
2021-02-16 13:41:49 -0700

    How to Change State Values in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -15,12 +15,20 @@ export default class PortfolioContainer extends Component {
                 {title: "SwingAway"}
             ]
         }
+
+        this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this)
     }
 
 
     portfolioItems() {
         return this.state.data.map(item => {
             return <PortfolioItem title={item.title} url={"google.com"}/>;
+        })
+    }
+
+    handlePageTitleUpdate() {
+        this.setState({
+            pageTitle: "something else"
         })
     }
 
@@ -31,6 +39,10 @@ export default class PortfolioContainer extends Component {
                 <h2>{this.state.pageTitle}</h2>
 
                 {this.portfolioItems()}
+
+                <hr/>
+
+                <button onClick={this.handlePageTitleUpdate}>Change Title</button>
             </div>
         )
     }
