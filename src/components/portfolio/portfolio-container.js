0dadbeac99d31af37da416a460f64ac8d9dbcf64
Benjamin Rivera <benriveracoding@gmail.com>
2021-02-17 11:55:14 -0700

    How to Use Ternary Operators in JSX Code to Implement Conditional Behavior

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,55 +1,60 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-import PortfolioItem from "./portfolio-item"
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super()
+  constructor() {
+    super();
 
-        this.state = {
-            pageTitle: "Welcome to my portfolio",
-            isLoading: false, 
-            data: [
-                {title: "Quip", category: "eCommerce"}, 
-                {title: "Eventbright", category: "Scheduling"}, 
-                {title: "Ministry Safe", category: "Enterprise"}, 
-                {title: "SwingAway", category: "eCommerce"}
-            ]
-        }
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: [
+        { title: "Quip", category: "eCommerce" },
+        { title: "Eventbright", category: "Scheduling" },
+        { title: "Ministry Safe", category: "Enterprise" },
+        { title: "SwingAway", category: "eCommerce" },
+      ],
+    };
 
-        this.handleFilter = this.handleFilter.bind(this)
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter((item) => {
+        return item.category === filter;
+      }),
+    });
+  }
+
+  portfolioItems() {
+    return this.state.data.map((item) => {
+      return <PortfolioItem title={item.title} url={"google.com"} />;
+    });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    handleFilter(filter) {
-      this.setState({
-        data: this.state.data.filter(item => {
-          return item.category === filter;
-        })
-      })
-    }
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
 
-    portfolioItems() {
-        return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"}/>;
-        })
-    }
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
-
-    render() {
-      if(this.state.isLoading) {
-        return <div>Loading...</div>
-      }
-
-      return(
-        <div>
-          <h2>{this.state.pageTitle}</h2>
-
-          <button onClick={() => this.handleFilter("eCommerce")}>eCommerce</button>
-          <button onClick={() => this.handleFilter("Scheduling")}>Scheduling</button>
-          <button onClick={() => this.handleFilter("Enterprise")}>Enterprise</button>
-
-          {this.portfolioItems()}
-        </div>
-      )
-    }
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
