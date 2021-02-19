c8b64600192e119f198dfefd765895ae3cd41021
Benjamin Rivera <benriveracoding@gmail.com>
2021-02-19 09:42:31 -0700

    Creating the Initial Login Component

---
+++ src/components/auth/login.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react"
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+        <form action="">
+          <input type="text"/>
+          <input type="password"/>
+        </form>
+
+      </div>
+    )
+  }
+}
