135e06e57196bf71cca69576803553e769527649
Benjamin Rivera <benriveracoding@gmail.com>
2021-02-23 14:20:00 -0700

    Populating the Portfolio Manager State with API Call Data

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,34 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: [],
+    };
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://benjibuns.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        })
+      })
+      .catch((error) => {
+        console.log("error in getPortfolioItems", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
   render() {
     return (
       <div className="portfolio-manager-wrapper">
@@ -12,6 +40,6 @@ export default class PortfolioManager extends Component {
           <h1>Portfolio sidebar</h1>
         </div>
       </div>
-    )
+    );
   }
 }
