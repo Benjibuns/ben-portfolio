763ae89b9d34607bac8dc5bfa2d2816db40bfef0
Benjamin Rivera <benriveracoding@gmail.com>
2021-03-01 14:46:41 -0700

    Rendering Blog Records to the Screen

--- src/components/app.js
+++ src/components/app.js
@@ -12,7 +12,7 @@ import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
-import Blog from "./pages/blogs";
+import Blog from "./pages/blog";
 import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
