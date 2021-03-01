0c31d579cb792388bdef6ea5802fceebee21ffd9
Benjamin Rivera <benriveracoding@gmail.com>
2021-03-01 15:26:17 -0700

    Rendering the Blog Details to the Screen

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -18,7 +18,9 @@ export default class BlogDetail extends Component {
         `https://benjibuns.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
       )
       .then((response) => {
-        console.log("response", response);
+        this.setState({
+          blogItem: response.data.portfolio_blog,
+        });
       })
       .catch((error) => {
         console.log("getBlogItem error", response);
@@ -30,9 +32,17 @@ export default class BlogDetail extends Component {
   }
 
   render() {
+    const {
+      title,
+      content,
+      featured_image_url,
+      blug_status,
+    } = this.state.blogItem;
     return (
       <div>
-        <h1>Blog detail</h1>
+        <h1>{title}</h1>
+        <img src={featured_image_url} />
+        <div>{content}</div>
       </div>
     );
   }
