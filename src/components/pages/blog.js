fb6da6d20d8698b68fe44944b125413f5541c519
Benjamin Rivera <benriveracoding@gmail.com>
2021-03-01 15:59:26 -0700

    Deep Dive: innerHeight, scrollTop, and offsetHeight

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -12,12 +12,17 @@ class Blog extends Component {
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
-    this.activateInfiniteScroll()
+    this.activateInfiniteScroll();
   }
 
   activateInfiniteScroll() {
     window.onscroll = () => {
-      console.log("onscroll");
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        console.log("get more posts");
+      }
     };
   }
 
