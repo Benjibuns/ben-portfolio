e45f72f21d92e7353741de7f2cc4b1f8b197ed83
Benjamin Rivera <benriveracoding@gmail.com>
2021-03-04 14:05:09 -0700

    Communicating with Blog Update API Endpoint

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -14,6 +14,8 @@ class BlogForm extends Component {
       blog_status: "",
       content: "",
       featured_image: "",
+      apiUrl: "https://benjibuns.devcamp.space/portfolio/portfolio_blogs",
+      apiAction: "post",
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -37,7 +39,7 @@ class BlogForm extends Component {
         { withCredentials: true }
       )
       .then((response) => {
-        this.props.handleFeaturedImageDelete()
+        this.props.handleFeaturedImageDelete();
       })
       .catch((error) => {
         console.log("deleteImage error", error);
@@ -49,7 +51,10 @@ class BlogForm extends Component {
       this.setState({
         id: this.props.blog.id,
         title: this.props.blog.title,
-        status: this.props.blog.status,
+        blog_status: this.props.blog.blog_status,
+        content: this.props.blog.content,
+        apiUrl: `https://benjibuns.devcamp.space/portfolio/portfolio_blogs/${this.props.blog.id}`,
+        apiAction: "patch",
       });
     }
   }
@@ -97,12 +102,12 @@ class BlogForm extends Component {
   }
 
   handleSubmit(event) {
-    axios
-      .post(
-        "https://benjibuns.devcamp.space/portfolio/portfolio_blogs",
-        this.buildForm(),
-        { withCredentials: true }
-      )
+    axios({
+      method: this.state.apiAction,
+      url: this.state.apiUrl,
+      data: this.buildForm(),
+      withCredentials: true,
+    })
       .then((response) => {
         if (this.state.featured_image) {
           this.featuredImageRef.current.dropzone.removeAllFiles();
@@ -116,7 +121,13 @@ class BlogForm extends Component {
           featured_image: "",
         });
 
-        this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
+        if (this.props.editMode) {
+          this.props.handleUpdateFormSubmission(response.data.portfolio_blog);
+        } else {
+          this.props.handleSuccessfulFormSubmission(
+            response.data.portfolio_blog
+          );
+        }
       })
       .catch((error) => {
         console.log("handleSubmit for blog error", error);
