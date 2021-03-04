597599d5939fb5ed4cebd575adb852fffc660290
Benjamin Rivera <benriveracoding@gmail.com>
2021-03-04 15:04:36 -0700

    Building About Page Styles for React Portfolio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,51 @@
-import React from "react"
+import React from "react";
+import profilPicture from "../../../static/assets/images/bio/coughing_cat.jpg";
 
-export default function() {
+export default function () {
   return (
-    <div>
-      About
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilPicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></div>
+      <div className="right-column">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, quidem
+        laudantium, dolorum nulla maxime soluta rerum ducimus quisquam
+        repudiandae laborum illum! Maxime soluta deleniti autem cumque labore
+        ipsam atque recusandae. Minus tenetur aspernatur hic quam facere rem,
+        doloremque, est possimus a sit minima delectus quos, molestias voluptas
+        ipsam suscipit recusandae enim! Modi impedit quasi laborum obcaecati
+        nisi consectetur quis fugiat! Sed alias libero veritatis dicta cumque
+        neque ipsam illum. Fugiat, ullam? Eos ea et assumenda illo beatae
+        delectus nisi, illum nam libero ducimus! Totam repellendus ex omnis id
+        facere amet! Harum perspiciatis omnis, maiores blanditiis deleniti
+        corporis impedit consequatur soluta animi architecto repellat dolorem
+        tempora aut natus tenetur accusamus atque earum alias! Labore dolorem
+        enim recusandae animi consequuntur laborum est! Veniam, asperiores
+        perspiciatis. Vel nemo ut vitae natus tenetur, enim quidem fugit est,
+        consequatur laboriosam voluptate neque? Tempore minima hic minus earum
+        eveniet laboriosam odit. Aliquam voluptatem eligendi fuga qui. Aliquid
+        rem cum libero aspernatur laboriosam provident id maxime recusandae
+        ratione explicabo doloremque quas, odio dolorem quos sed dolores autem!
+        Doloribus assumenda sed porro sit commodi eligendi explicabo, nostrum
+        ea! Amet dolorum, ab repellat rerum optio qui vero nulla quos non quis
+        rem ad aperiam aut, consequuntur, iusto enim maiores est explicabo
+        deserunt aspernatur laboriosam doloremque fuga. Esse, obcaecati ducimus.
+        Impedit dolorem ullam saepe molestias modi eaque perferendis debitis
+        commodi nobis, cumque ducimus officia alias sint illo aperiam quas
+        libero dolorum quidem porro nulla odio minus. Aliquid aperiam
+        exercitationem tenetur! Dignissimos et porro quae dolorum voluptates est
+        reiciendis incidunt ipsam doloribus quas ut, praesentium veniam, dolor,
+        dolores saepe nisi! Dolorem sapiente amet, quas minus totam vel placeat
+        fuga numquam animi! Sapiente molestiae dolores aperiam amet.
+        Voluptatibus repellendus eveniet est vitae quis facilis, rerum nihil id
+        dolorum perspiciatis doloremque magni sit tempora? Mollitia et
+        laboriosam nihil animi provident! Ipsum, iure sunt.
+      </div>
     </div>
-  )
+  );
 }
