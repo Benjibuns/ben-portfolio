0d2ef1c52f500dfb16e4c4a27b633dc06003c629
Benjamin Rivera <benriveracoding@gmail.com>
2021-03-05 11:07:40 -0700

    Full Filtering Feature Build Out in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import axios from "axios"
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
 
@@ -14,38 +14,46 @@ export default class PortfolioContainer extends Component {
     };
 
     this.handleFilter = this.handleFilter.bind(this);
-    this.getPortfolioItems = this.getPortfolioItems.bind(this)
+    this.getPortfolioItems = this.getPortfolioItems.bind(this);
   }
 
   handleFilter(filter) {
-    this.setState({
-      data: this.state.data.filter((item) => {
-        return item.category === filter;
-      }),
-    });
+    if (filter === "CLEAR_FILTERS") {
+      this.getPortfolioItems();
+    } else {
+      this.getPortfolioItems(filter);
+    }
   }
-  
-  getPortfolioItems() {
+
+  getPortfolioItems(filter = null) {
     axios
-    .get('https://benjibuns.devcamp.space/portfolio/portfolio_items')
-    .then(response => {
-      this.setState({
-        data: response.data.portfolio_items
+      .get("https://benjibuns.devcamp.space/portfolio/portfolio_items")
+      .then((response) => {
+        if (filter) {
+          this.setState({
+            data: response.data.portfolio_items.filter((item) => {
+              return item.category === filter;
+            }),
+          });
+        } else {
+          this.setState({
+            data: response.data.portfolio_items,
+          });
+        }
       })
-    })
-    .catch(error => {
-      console.log(error);
-    })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   portfolioItems() {
-    return this.state.data.map(item => {
-      return <PortfolioItem key={item.id} item={item}/>;
+    return this.state.data.map((item) => {
+      return <PortfolioItem key={item.id} item={item} />;
     });
   }
 
   UNSAFE_componentWillMount() {
-    this.getPortfolioItems()
+    this.getPortfolioItems();
   }
 
   render() {
@@ -54,18 +62,35 @@ export default class PortfolioContainer extends Component {
     }
 
     return (
-      <div className="portfolio-items-wrapper">
-        <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
-          eCommerce
-        </button>
-        <button className="btn" onClick={() => this.handleFilter("Scheduling")}>
-          Scheduling
-        </button>
-        <button className="btn" onClick={() => this.handleFilter("Enterprise")}>
-          Enterprise
-        </button>
+      <div className="homepage-wrapper">
+        <div className="filter-links">
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("CLEAR_FILTERS")}
+          >
+            All
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("eCommerce")}
+          >
+            eCommerce
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Scheduling")}
+          >
+            Scheduling
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Enterprise")}
+          >
+            Enterprise
+          </button>
 
-        {this.portfolioItems()}
+        </div>
+        <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
       </div>
     );
   }
